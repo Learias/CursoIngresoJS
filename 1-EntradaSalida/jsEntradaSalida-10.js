d82b180a6bf5e6e4d39a6af2bc445ed3d3217b29
Learias <lea.arias7@gmail.com>
2020-02-05 23:51:17 -0300

    Todos los ejercicios hechos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var sueldo;
+    var descuento;
+    var divisor;
+    var resultado;
+
+    descuento = 25
+    divisor = 100
+
+    sueldo = parseInt(document.getElementById("importe").value);
+
+    descuento = (sueldo * descuento) / divisor;
+    resultado = sueldo - descuento;
+
+    document.getElementById("resultado").value = resultado;
 }
