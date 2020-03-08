0ddfa67b3c1fbcfc756d22f7e43dd6c8f9cc9fcf
Leandro Arias <lea.arias7@gmail.comgit config --global user.name Leandro>
2020-03-07 21:37:14 -0300

    Hechos

--- Nuevo/dos.js
+++ Nuevo/dos.js
@@ -1,77 +1,84 @@
 function mostrar() {
+
   var respuesta = 's';
   var nombreingrediente;
   var peso;
   var precio;
   var tipoingrediente;
+  var preciobruto;
   var acumuladorpeso = 0;
   var acumuladortotal = 0;
   var acumuladorprecio = 0;
-  var acumuladorpreciobruto = 0;
-  var preciocondescuento = 0;
+  var preciocondescuento = "no hubo descuento";
   var descuento;
   var promedioprecio;
   var preciomax;
-  var ingredientemax;
+  var tipoingredientemax;
   var contador = 0;
-  var flag = 0;
-
 
 
 
   do {
 
-    nombreingrediente = prompt('Ingrese el nombre del ingrediente');
-  } while (!isNaN(nombreingrediente));
+    do {
 
-  do {
-    peso = prompt('ingrese peso  (entre 10 y 1000) ');
-    peso = parseInt(peso);
-  } while (isNaN(peso) || peso < 10 || peso > 1000);
+      nombreingrediente = prompt('Ingrese el nombre del ingrediente');
+    } while (!isNaN(nombreingrediente));
 
-  do {
-    precio = prompt('ingrese precio por kg  (Mas de 0) ');
-    precio = parseInt(precio);
-  } while (isNaN(precio) || precio < 1);
+    do {
+      peso = prompt('ingrese peso  (entre 10 y 1000) ');
+      peso = parseInt(peso);
+    } while (isNaN(peso) || peso < 10 || peso > 1000);
 
-  do {
-    tipoingrediente = prompt('Ingrese el tipo de ingrediente  ("v" para vegetal, "a" para animal, "m" para mezcla)');
-  } while (!isNaN(tipoingrediente) || tipoingrediente != 'v' && tipoingrediente != 'a' && tipoingrediente != 'm');
+    do {
+      precio = prompt('ingrese precio por kg  (Mas de 0) ');
+      precio = parseInt(precio);
+    } while (isNaN(precio) || precio < 1);
 
-  acumuladorpeso = acumuladorpeso + peso;
-  acumuladorprecio = acumuladorprecio + precio;
-  acumuladortotal = acumuladorprecio + acumuladortotal
-  contador++;
+    do {
+      tipoingrediente = prompt('Ingrese el tipo de ingrediente  ("v" para vegetal, "a" para animal, "m" para mezcla)');
+    } while (!isNaN(tipoingrediente) || tipoingrediente != 'v' && tipoingrediente != 'a' && tipoingrediente != 'm');
 
-  if (flag == 0 || preciomax < precio) {
-    preciomax = precio
-    ingredientemax = tipoingrediente;
-    flag++;
+    acumuladorpeso = acumuladorpeso + peso;
+    acumuladorprecio = acumuladorprecio + precio;
+    acumuladortotal = precio * peso + acumuladortotal;
+
+
+    if (contador == 0 || preciomax < precio) {
+      preciomax = precio;
+      tipoingredientemax = tipoingrediente;
+
+    }
+
+    contador++;
+
+    respuesta = prompt('Quiere continuar?');
+
+  } while (respuesta == 's' || respuesta == 'si' || respuesta == 'Si' || respuesta == 'SI');
+
+  if (acumuladorpeso > 100 && acumuladorpeso < 300) {
+
+    descuento = acumuladortotal * 15 / 100;
+    preciocondescuento = acumuladortotal - descuento;
+
+  } else if (acumuladorpeso < 100) {
+
+    descuento = 0
+
+  } else {
+
+    descuento = acumuladortotal * 25 / 100;
+    preciocondescuento = acumuladortotal - descuento;
+
+  }
+  preciobruto = acumuladortotal;
+  if (acumuladorprecio != 0) {
+    promedioprecio = acumuladorprecio / contador;
   }
 
-  respuesta = prompt('Quiere continuar?');
-  
- while (respuesta == 's' || respuesta == 'si' || respueta == 'Si' || respuesta == 'SI');
-
-if (acumuladorpeso > 100 && acumuladorpeso < 300) {
-
-  descuento = (acumuladorpeso * acumuladorprecio) * 15 / 100;
-
-} else if (acumuladorpeso > 300) {
-
-  descuento = (acumuladorpeso * acumuladorprecio) * 25 / 100;
-
-} else {
-
-  descuento = 0
-}
-
-preciocondescuento = acumuladorpeso * precio - descuento
-promedioprecio = acumuladortotal / contador
-
-document.write('a) el precio bruto a abonar es: ' + acumuladorprecio);
-document.write('b) el precio con descuento es: ' + preciocondescuento);
-document.write('d) el ingrediente mas caro es: ' + preciomax + ' y el ingrediente es ' + ingredientemax);
-ocument.write('f) el de precio por kg es' + promedioprecio);
+  document.write('a) el precio bruto a abonar es: ' + preciobruto + '</br>');
+  document.write('b) el precio con descuento es: ' + preciocondescuento + '</br>');
+  document.write('d) el valor del ingrediente mas caro es: ' + preciomax + ' y el tipo de ingrediente es ' + tipoingredientemax + '</br>');
+  document.write('f) el promedio de precio por kg total es: ' + promedioprecio);
 
 }
--- Nuevo/tres.js
+++ Nuevo/tres.js
@@ -1,23 +1,99 @@
 function mostrar() {
+	var respuesta = 's';
 	var edad;
 	var nombre;
 	var sexo;
 	var estadocivil;
+	var hombrecasadomasjoven;
+	var nombrehombrecasadomasjoven;
+	var sexopasajeromasviejo = '-';
+	var nombrepasajeromasviejo = '-';
+	var edadpasajeromasviejo;
+	var contadorcasadasviudas = 0;
+	var acumuladoredadmujeres = 0;
+	var contadormujeres = 0;
+	var contadorhombressolteros = 0;
+	var acumuladoredadhombressolteros = 0;
+	var promedioedadmujeres = '-';
+	var promedioedadhombresolteros = '-';
+	var contador = 0;
+	var flag = 0;
 
-
-	nombre = prompt('ingrese su nombre');
 
 	do {
-		edad = prompt('ingrese edad  (mayor a 18 y menor a 107) ');
-		edad = parseInt(edad);
-	} while (isNaN(edad) || edad < 18 || edad > 107);
 
-	do {
-		sexo = prompt('Ingrese el tipo de sexo ("f" para fememino o "m" para masculino)');
-	} while (!isNaN(sexo) || sexo != 'f' && sexo != 'm');
+		nombre = prompt('ingrese su nombre');
 
-	do {
-		estadocivil = prompt('Ingrese su estado civil ("soltero" / "casado / "viudo")');
-	} while (!isNaN(estadocivil) || estadocivil != 'f' && estadocivil != 'm');
+		do {
+			edad = prompt('ingrese edad  (mayor a 18 y menor a 107) ');
+			edad = parseInt(edad);
+		} while (isNaN(edad) || edad < 18 || edad > 107);
+
+		do {
+			sexo = prompt('Ingrese el tipo de sexo ("f" para fememino o "m" para masculino)');
+		} while (!isNaN(sexo) || sexo != 'f' && sexo != 'm');
+
+		do {
+			estadocivil = prompt('Ingrese su estado civil ("soltero" / "casado / "viudo")');
+		} while (!isNaN(estadocivil) || estadocivil != 'soltero' && estadocivil != 'casado' && estadocivil != 'viudo');
+
+
+
+		// sexo del pasajero más viejo y su nombre
+
+		if (edadpasajeromasviejo < edad || flag == 0){
+
+		  sexopasajeromasviejo = sexo;
+		  edadpasajeromasviejo = edad;
+		  nombrepasajeromasviejo = nombre;
+		  flag++;
+		}
+
+		if ((estadocivil == 'casado' && hombrecasadomasjoven < edad) || contador == 0) {
+			hombrecasadomasjoven = edad;
+			nombrehombrecasadomasjoven = nombre;
+			contador++;
+		}
+
+		
+		// suma edad de mujeres casadas o viudas + cantidad de casadas y viudas
+
+		switch (sexo) {
+			case ('f'):
+				acumuladoredadmujeres = acumuladoredadmujeres + edad;
+				contadormujeres++;
+				if (estadocivil == 'casado' || estadocivil == 'viudo') {
+					contadorcasadasviudas++;
+				}
+				break;
+
+
+			// suma edad hombres solteros + cantidad hombres solteros
+			default:
+
+				if (estadocivil == 'soltero') {
+					acumuladoredadhombressolteros = acumuladoredadhombressolteros + edad;
+					contadorhombressolteros++;
+				}
+			}
+
+		respuesta = prompt('Quiere continuar? ');
+
+	} while (respuesta == 's' || respuesta == 'Si' || respuesta == 'SI' || respuesta == 'si');
+
+	if (acumuladoredadmujeres != 0) {
+		promedioedadmujeres = acumuladoredadmujeres / contadormujeres;
+	}
+
+	if (acumuladoredadhombressolteros != 0) {
+		promedioedadhombresolteros = acumuladoredadhombressolteros / contadorhombressolteros;
+	}
+
+
+	document.write('a) el nombre del hombre casado mas joven es: ' + nombrehombrecasadomasjoven + '</br>');
+	document.write('b) el sexo del pasajero mas viejo es: ' + sexopasajeromasviejo + ' y su nombre es ' + nombrepasajeromasviejo + '</br>');
+	document.write('c) la cantidad de mujeres casadas o viudas es ' + contadorcasadasviudas + '</br>');
+	document.write('d) el promedio de edad entras las mujeres es: ' + promedioedadmujeres + '</br>');
+	document.write('e) el promedio de edad entre los hombres solteros es: ' + promedioedadhombresolteros);
 
 }
