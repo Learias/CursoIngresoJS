52d428e022aea48a2a6c85aac64f2377b687644c
Leandro Arias <lea.arias7@gmail.comgit config --global user.name Leandro>
2020-03-09 18:53:57 -0300

    hechos

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,99 @@
-function mostrar()
-{
-  
+function mostrar() {
+    var respuesta = 's';
+    var nombrehuesped;
+    var cantidaddepersonas;
+    var cantidaddediasdeestadia;
+    var formasdepago;
+    var cantidaddepersonasmaxhuesped;
+    var nombrehuespedmaxcantidad;
+    var mascantidaddedias;
+    var cantidadpersonasmasdias;
+    var contadortarjeta = 0;
+    var contadorefectivo = 0;
+    var contadorqr = 0;
+    var acumuladordediasreserva = 0;
+    var formadepagomax;
+    var contador = 0;
+    var promedio;
+
+    do {
+        do {
+            nombrehuesped = prompt('ingrese el nombre del huesped');
+        } while (!isNaN(nombrehuesped));
+
+        do {
+            cantidaddepersonas = prompt('ingrese la cantidad de personas');
+            cantidaddepersonas = parseInt(cantidaddepersonas);
+        } while (isNaN(cantidaddepersonas) || cantidaddepersonas < 1);
+
+        do {
+            cantidaddediasdeestadia = prompt('ingrese la cantidad de dias de estadia');
+            cantidaddediasdeestadia = parseInt(cantidaddediasdeestadia);
+        } while (isNaN(cantidaddediasdeestadia) || cantidaddediasdeestadia < 1);
+
+        do {
+            formasdepago = prompt('ingrese la forma de pago (tarjeta / efectivo / QR)');
+        } while (!isNaN(formasdepago || formasdepago != 'tarjeta' && formasdepago != 'efectivo' && formasdepago != 'QR'));
+
+
+        // Huesped que trajo mayor cantidad de personas 
+
+        if (contador == 0 || cantidaddepersonasmaxhuesped < cantidaddepersonas) {
+
+            cantidaddepersonasmaxhuesped = cantidaddepersonas
+            nombrehuespedmaxcantidad = nombrehuesped
+        }
+
+        // La cantidad de personas que se quedaron más días
+
+        if (contador == 0 || mascantidaddedias < cantidaddediasdeestadia) {
+
+            mascantidaddedias = cantidaddediasdeestadia;
+            cantidadpersonasmasdias = cantidaddepersonas;
+
+        }
+
+        // Forma de pago má utilizada
+
+        switch (formasdepago) {
+            case ('tarjeta'):
+                contadortarjeta++;
+                break;
+            case ('efectivo'):
+                contadorefectivo++;
+                break;
+            default:
+                contadorqr++;
+        }
+
+        if ((contadortarjeta > contadorefectivo) && (contadortarjeta > contadorqr)) {
+            formadepagomax = formasdepago
+        }
+        if ((contadorefectivo > contadortarjeta) && (contadorefectivo > contadorqr)) {
+            formadepagomax = formasdepago
+        }
+        if ((contadorqr > contadortarjeta) && (contadorqr > contadoefectivo)) {
+            formadepagomax = formasdepago
+        }
+        //(A CHEQUEAR, ULTIMO PUNTO RESTANTE)
+
+        // cantidad de dias de reserva en total
+        acumuladordediasreserva = acumuladordediasreserva + cantidaddediasdeestadia;
+        contador++;
+
+
+
+        respuesta = prompt('Quiere continuar?');
+    } while (respuesta == 's');
+
+
+    if (acumuladordediasreserva != 0) {
+        promedio = acumuladordediasreserva / contador;
+    }
+
+    document.write('a) El huesped que trajo más personas en una sola reserva es: ' + nombrehuespedmaxcantidad + '</br>');
+    document.write('b) La cantidad de personas que se quedaron más días son: ' + cantidadpersonasmasdias + '</br>');
+    document.write('c) La forma de pago mas utilizada es: ' + formadepagomax + '</br>');
+    document.write('d) El promedio de días por cantidad de reserva es: ' + promedio);
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,118 @@
+/*
+Ejercicio práctica
 
-function mostrar()
-{
+Se pide la carga de datos de un contenedor  de alimento canino 
+con un máximo de carga de 1000kg, cargar hasta que el cliente quiera o se llene el contenedor.
+
+
+se pide:
+1)marca
+2)kilos por bolsa
+3)cantidad de bolsas
+4)importe por bolsa
+
+a)que marca tiene más kilos en el contenedor
+b)que marca tiene más bolsas de alimento en el contenedor
+c)que marca tiene el mayor importe por bolsa de alimento
+d)el importe y la marca de la bolsa de alimento menos pesada.
+
+*/
+
+
+
+
+
+
+
+function mostrar() {
+
+    var respuesta = 's'
+    var marca;
+    var kgbolsas;
+    var precioporbolsa;
+    var cantidadbolsas;
+    var acumuladorcontenedor = 0;
+    var kgbolsasmax;
+    var marcamax;
+    var flag = 0;
+    var cantidadbolsasmax;
+    var marcacantidadbolsasmax;
+    var precioporbolsamax;
+    var marcaprecioporbolsamax;
+    var kgbolsasmin;
+    var marcakgbolsasmin;
+    var importekgbolsasmin;
+
+
+    do {
+
+        do {
+            marca = prompt('ingrese la marca del producto');
+        } while (!isNaN(marca));
+        do {
+            kgbolsas = prompt('ingrese el peso de la bolsa en kg (a partir de 1kg)');
+            kgbolsas = parseInt(kgbolsas);
+        } while (isNaN(kgbolsas) || kgbolsas < 1);
+        do {
+            cantidadbolsas = prompt('ingrese la cantidad de bolsas (a partir de 1)');
+            cantidadbolsas = parseInt(cantidadbolsas);
+        } while (isNaN(cantidadbolsas) || cantidadbolsas < 1);
+        do {
+            precioporbolsa = prompt('ingrese el precio de la bolsa');
+            precioporbolsa = parseInt(precioporbolsa);
+        } while (isNaN(precioporbolsa) || precioporbolsa < 1);
+
+        // total de kg dentro del contenedor
+
+        acumuladorcontenedor = kgbolsas + acumuladorcontenedor
+
+        // marca con mas kg en el contenedor
+
+        if (flag == 0 || kgbolsasmax < kgbolsas) {
+
+            kgbolsasmax = kgbolsas;
+            marcamax = marca;
+
+        }
+
+        // marca con mayor cantidad de bolsas en el contenedor
+
+        if (flag == 0 || cantidadbolsasmax < cantidadbolsas) {
+
+            cantidadbolsasmax = cantidadbolsas;
+            marcacantidadbolsasmax = marca;
+
+        }
+
+        // marca con mayor importe por bolsa
+
+        if (flag == 0 || precioporbolsamax < precioporbolsa) {
+
+            precioporbolsamax = precioporbolsa;
+            marcaprecioporbolsamax = marca;
+
+        }
+
+        // marca con menor peso de bolsa y su importe
+
+        if (flag == 0 || kgbolsasmin > kgbolsas) {
+
+            kgbolsasmin = kgbolsas;
+            marcakgbolsasmin = marca;
+            importekgbolsasmin = precioporbolsa
+            flag++;
+        }
+
+
+
+        respuesta = prompt('quiere continuar?');
+    } while (respuesta == 's' && acumuladorcontenedor < 1000);
+
+
+
+    document.write('a) la marca con mas kg en el contenedor es: ' + marcamax + '</br>');
+    document.write('b) la marca con mas bolsas en el contenedor es: ' + marcacantidadbolsasmax + '</br>');
+    document.write('c) la marca con el mayor importe por bolsa es: ' + marcaprecioporbolsamax + '</br>');
+    document.write('d) la marca de alimento menos pesada es: ' + marcakgbolsasmin + ' y su importe es: $' + importekgbolsasmin + '</br>');
 
 }
